923b1418da133fbe753eb61d1c94746f1214df7f
Venkat Yadlapati <venkat.yadlapati@hotmail.com>
2022-11-27 00:27:53 +0530

    Add Basic layout and dark mode fucitonality

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.watbtn = new System.Windows.Forms.Button();
+            this.snabtn = new System.Windows.Forms.Button();
+            this.rstbtn = new System.Windows.Forms.Button();
+            this.hlpbtn = new System.Windows.Forms.Button();
+            this.darkbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // watbtn
+            // 
+            this.watbtn.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.watbtn.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.watbtn.Location = new System.Drawing.Point(347, 651);
+            this.watbtn.Name = "watbtn";
+            this.watbtn.Padding = new System.Windows.Forms.Padding(4);
+            this.watbtn.Size = new System.Drawing.Size(200, 100);
+            this.watbtn.TabIndex = 1;
+            this.watbtn.Text = "WATER";
+            this.watbtn.UseVisualStyleBackColor = true;
+            // 
+            // snabtn
+            // 
+            this.snabtn.Location = new System.Drawing.Point(587, 651);
+            this.snabtn.Name = "snabtn";
+            this.snabtn.Padding = new System.Windows.Forms.Padding(4);
+            this.snabtn.Size = new System.Drawing.Size(200, 100);
+            this.snabtn.TabIndex = 2;
+            this.snabtn.Text = "SNACK";
+            this.snabtn.UseVisualStyleBackColor = true;
+            // 
+            // rstbtn
+            // 
+            this.rstbtn.Location = new System.Drawing.Point(827, 651);
+            this.rstbtn.Name = "rstbtn";
+            this.rstbtn.Padding = new System.Windows.Forms.Padding(4);
+            this.rstbtn.Size = new System.Drawing.Size(200, 100);
+            this.rstbtn.TabIndex = 3;
+            this.rstbtn.Text = "REST ROOM";
+            this.rstbtn.UseVisualStyleBackColor = true;
+            // 
+            // hlpbtn
+            // 
+            this.hlpbtn.Location = new System.Drawing.Point(1067, 651);
+            this.hlpbtn.Name = "hlpbtn";
+            this.hlpbtn.Padding = new System.Windows.Forms.Padding(4);
+            this.hlpbtn.Size = new System.Drawing.Size(200, 100);
+            this.hlpbtn.TabIndex = 4;
+            this.hlpbtn.Text = "HELP";
+            this.hlpbtn.UseVisualStyleBackColor = true;
+            this.hlpbtn.Click += new System.EventHandler(this.hlpbtn_Click);
+            // 
+            // darkbtn
+            // 
+            this.darkbtn.Location = new System.Drawing.Point(1288, 861);
+            this.darkbtn.Name = "darkbtn";
+            this.darkbtn.Size = new System.Drawing.Size(150, 46);
+            this.darkbtn.TabIndex = 5;
+            this.darkbtn.Text = "Dark Mode";
+            this.darkbtn.UseVisualStyleBackColor = true;
+            this.darkbtn.Click += new System.EventHandler(this.darkbtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1524, 959);
+            this.Controls.Add(this.darkbtn);
+            this.Controls.Add(this.hlpbtn);
+            this.Controls.Add(this.rstbtn);
+            this.Controls.Add(this.snabtn);
+            this.Controls.Add(this.watbtn);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private Button watbtn;
+        private Button snabtn;
+        private Button rstbtn;
+        private Button hlpbtn;
+        private Button darkbtn;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,28 @@ namespace omron
         {
             InitializeComponent();
         }
+
+        private void hlpbtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool isClicked = false;
+        private void darkbtn_Click(object sender, EventArgs e)
+        {
+
+            if (isClicked)
+            {
+                this.BackColor = Color.White;
+                darkbtn.Text = "Dark Mode";
+                isClicked = false;
+            }
+            else
+            {
+                this.BackColor = Color.FromArgb(64, 66, 88);
+                darkbtn.Text = "Light Mode";
+                isClicked = true;
+            }
+        }
     }
 }
